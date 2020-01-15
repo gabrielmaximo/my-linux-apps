8d87ac919785d4650065c429e96ddc6f6969d4a5
Gabriel Maximo <maximo@ufu.br>
2020-01-15 10:49:33 -0300

    Update workstation.sh

--- workstation.sh
+++ workstation.sh
@@ -26,23 +26,23 @@ sudo snap install discord &&
 sudo snap install whatsdesk &&
 sudo snap install telegram-desktop &&
 
-curl https://github.com/egoist/devdocs-app/releases/download/v0.6.9/DevDocs_0.6.9_amd64.deb &&
-apt install ./DevDocs_0.6.9_amd64.deb -y &&
+## Softwares que precisam de download externo ##
+
+## curl https://github.com/egoist/devdocs-app/releases/download/v0.6.9/DevDocs_0.6.9_amd64.deb &&
+## sudo apt install ./DevDocs_0.6.9_amd64.deb -y &&
 sudo add-apt-repository multiverse &&
 sudo apt install steam &&
 
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add - &&
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list &&
+## curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add - &&
+## echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list &&
 
-sudo apt install yarn -y &&
+## sudo apt install yarn -y &&
  
-wget "https://dl.strem.io/linux/v4.4.54/stremio_4.4.52-1_amd64.deb" -O stremio.appimage &&
-chmod +x stremio.appimage &&
-sudo apt install ./stremio.appimage -y &&
+## wget "https://dl.strem.io/linux/v4.4.54/stremio_4.4.52-1_amd64.deb" -O stremio.appimage &&
+## chmod +x stremio.appimage &&
+## sudo apt install ./stremio.appimage -y &&
 
-## Softwares que precisam de download externo ##
-
-cd ~/Downloads/ && wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo dpkg -i *.deb && wget -c https://uploads.treeunfe.me/downloads/instalar-freenfe.exe &&
+## cd ~/Downloads/ && wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb && sudo dpkg -i *.deb && wget -c https://uploads.treeunfe.me/downloads/instalar-freenfe.exe &&
 
 ## Atualização do sistema ##
 
